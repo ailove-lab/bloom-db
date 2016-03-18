87e75907fb51de880d2073e62a4fb12e1577c6db
peko <gasu.basu@gmail.com>
2016-03-18 13:06:16 +0300

    update

--- timer.c
+++ timer.c
@@ -13,6 +13,6 @@ void timer_stop() {
     struct timeval tm2;
     gettimeofday(&tm2, NULL);
 
-    unsigned long long t = 1000 * (tm2.tv_sec - tm1.tv_sec) + (tm2.tv_usec - tm1.tv_usec) / 1000;
-    fprintf(stderr, "%llu ms\n", t);
+    last_timer = 1000 * (tm2.tv_sec - tm1.tv_sec) + (tm2.tv_usec - tm1.tv_usec) / 1000;
+    fprintf(stderr, "%llu ms\n", last_timer);
 }
